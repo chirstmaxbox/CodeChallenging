805cc67d07fd4d5e92450876e156db475f4c82fe
anh <anh_tranquoc@outlook.com>
2021-02-27 18:38:01 -0500

    Step 2

---
+++ anh_vog_backend_codechallenge/VogCodeChallenge.API/Models/Entities/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.API.Models.Entities
+{
+	public class Department
+	{
+		public string Id { get; set; }
+		public string Name { get; set; }
+		public String Address { get; set; }
+		public List<Employee> Employees { get; set; }
+	}
+}
---
+++ anh_vog_backend_codechallenge/VogCodeChallenge.API/Models/Entities/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.API.Models.Entities
+{
+	public class Employee
+	{
+		public int EmployeeId { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public String JobTitle { get; set; }
+		public String Address { get; set; }
+		public int DeptId { get; set; }
+	}
+}
