82ffb43483b0b0f198bbcd4b8ecc576749f8f98d
anh <anh_tranquoc@outlook.com>
2021-02-27 20:44:12 -0500

    step7

---
+++ anh_vog_backend_codechallenge/ConsoleProject/QuestionClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleProject
+{
+	class QuestionClass
+	{
+		static List<string> NamesList = new List<string>()
+		{
+			"Jimmy",
+			"Jeffrey",
+			"John",
+		};
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Value of NamesList");
+			Console.WriteLine(string.Join(Environment.NewLine, NamesList));
+		}
+	}
+}
